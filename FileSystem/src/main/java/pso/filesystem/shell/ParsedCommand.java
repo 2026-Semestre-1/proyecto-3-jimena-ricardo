6d85835dd4806deac86c09bca5210265f340cfa6
Ricardo Arce <yasserarce2404@gmail.com>
2026-06-23 15:51:08 -0600

    fix: remove old debug print

--- FileSystem/src/main/java/pso/filesystem/shell/ParsedCommand.java
+++ FileSystem/src/main/java/pso/filesystem/shell/ParsedCommand.java
@@ -1,9 +1,4 @@
 package pso.filesystem.shell;
 
-import java.util.Arrays;
-
 public record ParsedCommand(String name, String[] options, String[] operands) {
-    public void debugPrint() {
-        System.out.println("name: " + name + ", options: " + Arrays.toString(options) + ", operands: " + Arrays.toString(operands));
-    }
 }
