fe26105a26224f4a6e15c7543a11a5fb30a97476
Ricardo Arce <yasserarce2404@gmail.com>
2026-06-24 11:01:58 -0600

    feat: add FileSystem class to have a centralized access point to structures

--- FileSystem/src/main/java/pso/filesystem/FileSystem.java
+++ FileSystem/src/main/java/pso/filesystem/FileSystem.java
@@ -1,12 +1,36 @@
 package pso.filesystem;
 
-import pso.filesystem.shell.Shell;
+import java.io.IOException;
 
-public class FileSystem {
+public final class FileSystem {
 
-    public static void main(String[] args) {
-        System.out.println("File system started.");
-        Shell shell = new Shell();
-        shell.start();
+    private final String diskName;
+    private final BootBlock bootBlock;
+    private final SuperBlock superBlock;
+
+    private FileSystem(String diskName, BootBlock bootBlock, SuperBlock superBlock) {
+        this.diskName = diskName;
+        this.bootBlock = bootBlock;
+        this.superBlock = superBlock;
+    }
+
+    public static FileSystem mount(String diskName) throws IOException {
+        try (VirtualDisk disk = VirtualDisk.openReadOnly(diskName, BootBlock.BINARY_SIZE)) {
+            BootBlock bootBlock = BootBlock.fromBytes(disk.readBlock(0));
+            SuperBlock superBlock = SuperBlock.fromBytes(disk.readBlock(bootBlock.superBlockIndex()));
+            return new FileSystem(diskName, bootBlock, superBlock);
+        }
+    }
+
+    public String diskName() {
+        return diskName;
+    }
+
+    public BootBlock bootBlock() {
+        return bootBlock;
+    }
+
+    public SuperBlock superBlock() {
+        return superBlock;
     }
 }
---
+++ FileSystem/src/main/java/pso/filesystem/Main.java
@@ -0,0 +1,12 @@
+package pso.filesystem;
+
+import pso.filesystem.shell.Shell;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome bro. :D");
+        Shell shell = new Shell();
+        shell.start();
+    }
+}
