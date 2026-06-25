bf8600e785e0d8d7288ae91b94725f139e6b6c2e
jimendezm <jimenamendezmorales@gmail.com>
2026-06-25 17:33:50 -0600

    feat: disk visualizer

--- FileSystem/src/main/java/pso/filesystem/Main.java
+++ FileSystem/src/main/java/pso/filesystem/Main.java
@@ -1,12 +1,18 @@
 package pso.filesystem;
 
-import pso.filesystem.shell.Shell;
+import pso.ui.MainFrame;
+import javax.swing.SwingUtilities;
 
 public class Main {
 
     public static void main(String[] args) {
+        /* Sorry Ricardo, soon you can use the UI
         System.out.println("Welcome bro. :D");
         Shell shell = new Shell();
-        shell.start();
+        shell.start();*/
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
     }
 }
